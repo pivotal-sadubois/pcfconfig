55cfbb32453eb731434acd01e52429a87da36d02
Sacha Dubois <sadubois@pivotal.io>
2019-07-18 21:07:15 +0200

    fixed aws, gcp checks

--- scripts/jumpInstallUtilities.sh
+++ scripts/jumpInstallUtilities.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+export LC_ALL=en_US.UTF-8
+
 echo "Install Software on Jumphost"
 echo "- Update GIT repo https://github.com/pivotal-sadubois/pcfconfig.git"
 (cd /home/ubuntu/pcfconfig; git fetch)
 echo "- Install bsdutils"
-sudo apt-get install bsdutils
+sudo apt-get install bsdutils -y
+sudo apt install python-pip -y
+sudo apt install python3-pip -y
+pip install --upgrade pip
+sudo pip3 install awscli --upgrade 
+sudo wget -q -O - https://raw.githubusercontent.com/starkandwayne/homebrew-cf/master/public.key | sudo  apt-key add -
+sudo echo "deb http://apt.starkandwayne.com stable main" | sudo  tee /etc/apt/sources.list.d/starkandwayne.list
+sudo apt-get update
+sudo apt-get install om -y
