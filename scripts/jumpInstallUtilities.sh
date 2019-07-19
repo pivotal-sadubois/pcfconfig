4d49d1e97908af18e4e00b8f7d0fff354567ce6b
Sacha Dubois <sadubois@pivotal.io>
2019-07-19 20:13:04 +0200

    fixed aws, gcp checks

--- scripts/jumpInstallUtilities.sh
+++ scripts/jumpInstallUtilities.sh
@@ -5,13 +5,29 @@ export LC_ALL=en_US.UTF-8
 echo "Install Software on Jumphost"
 echo "- Update GIT repo https://github.com/pivotal-sadubois/pcfconfig.git"
 (cd /home/ubuntu/pcfconfig; git fetch)
-echo "- Install bsdutils"
-sudo apt-get install bsdutils -y
-sudo apt install python-pip -y
-sudo apt install python3-pip -y
-pip install --upgrade pip
-sudo pip3 install awscli --upgrade 
-sudo wget -q -O - https://raw.githubusercontent.com/starkandwayne/homebrew-cf/master/public.key | sudo  apt-key add -
-sudo echo "deb http://apt.starkandwayne.com stable main" | sudo  tee /etc/apt/sources.list.d/starkandwayne.list
-sudo apt-get update
-sudo apt-get install om -y
+
+echo "- Install AWS CLI"
+if [ ! -x /usr/local/bin/aws ]; then 
+  sudo apt install python-pip -y
+  sudo apt install python3-pip -y
+  pip install --upgrade pip
+  sudo pip3 install awscli --upgrade 
+fi
+
+echo "- Install OM"
+if [ ! -x /usr/bin/om ]; then 
+  sudo wget -q -O - https://raw.githubusercontent.com/starkandwayne/homebrew-cf/master/public.key | sudo  apt-key add -
+  sudo echo "deb http://apt.starkandwayne.com stable main" | sudo  tee /etc/apt/sources.list.d/starkandwayne.list
+  sudo apt-get update
+  sudo apt-get install om -y
+fi
+
+echo "- Install JQ"
+if [ ! -x /usr/bin/jq ]; then 
+  sudo apt-get install jq -y
+fi
+
+echo "- Install Terraform"
+if [ ! -x /usr/bin/terraform ]; then 
+  sudo apt-get install terraform -y
+fi
