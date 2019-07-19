b0e7ba33f20db5f6bad89edb3196a9834bfbe903
Sacha Dubois <sadubois@pivotal.io>
2019-07-19 21:46:22 +0200

    fixed aws, gcp checks

--- scripts/jumpInstallUtilities.sh
+++ scripts/jumpInstallUtilities.sh
@@ -6,28 +6,33 @@ echo "Install Software on Jumphost"
 echo "- Update GIT repo https://github.com/pivotal-sadubois/pcfconfig.git"
 (cd /home/ubuntu/pcfconfig; git fetch)
 
-echo "- Install AWS CLI"
 if [ ! -x /usr/local/bin/aws ]; then 
+  echo "- Install AWS CLI"
   sudo apt install python-pip -y
   sudo apt install python3-pip -y
   pip install --upgrade pip
   sudo pip3 install awscli --upgrade 
 fi
 
-echo "- Install OM"
 if [ ! -x /usr/bin/om ]; then 
+  echo "- Install OM"
   sudo wget -q -O - https://raw.githubusercontent.com/starkandwayne/homebrew-cf/master/public.key | sudo  apt-key add -
   sudo echo "deb http://apt.starkandwayne.com stable main" | sudo  tee /etc/apt/sources.list.d/starkandwayne.list
   sudo apt-get update
   sudo apt-get install om -y
 fi
 
-echo "- Install JQ"
 if [ ! -x /usr/bin/jq ]; then 
+  echo "- Install JQ"
   sudo apt-get install jq -y
 fi
 
-echo "- Install Terraform"
 if [ ! -x /usr/bin/terraform ]; then 
+  echo "- Install Terraform"
   sudo apt-get install terraform -y
+fi
+
+if [ ! -x /usr/bin/zipinfo ]; then 
+  echo "- Install ZIP"
+  sudo apt-get install zip -y
 fi
