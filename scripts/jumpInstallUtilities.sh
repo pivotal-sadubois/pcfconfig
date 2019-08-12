d72f8c26bbb5d3ceb0602f2983fa4ea78dbc4412
Sacha Dubois <sadubois@pivotal.io>
2019-08-13 01:19:00 +0200

    added GCP jump config

--- scripts/jumpInstallUtilities.sh
+++ scripts/jumpInstallUtilities.sh
@@ -22,11 +22,15 @@ while  [ ! -x /usr/bin/gcloud ]; do
   sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list > /dev/null
   apt-get install apt-transport-https ca-certificates -y > /dev/null 2>&1
 
+echo gaga1
   echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | \
   sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list > /dev/null
+echo gaga2
   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg 2>/dev/null | \
   sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add - 
+echo gaga3
   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg 2>/dev/null | sudo apt-key add -
+echo gaga4
   sudo apt-get update && sudo apt-get install google-cloud-sdk -y > /dev/null 2>&1
 done
 
