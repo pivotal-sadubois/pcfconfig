f36cf4c70bea6fdb5aa62631400554003617aac3
Sacha Dubois <sadubois@pivotal.io>
2019-08-13 20:47:36 +0200

    added GCP jump config

---
+++ pcfconfig.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd $HOME
+if [ -f ~/nohup.out ]; then
+  cnt=$(egrep -c "################################ EOF ################################" ~/nohup.out)
+  if [ $cnt -gt 0 ]; then 
+    cat ~/nohup.out
+    exit 0
+  fi
+fi
+
+if [ -f ~/pcfconfig.pid ]; then
+  read pid < ~/pcfconfig.pid
+  cnt=$(ps -p $pid -o pid,comm | sed 1d | grep -c " ${pid} ") 
+  if [ $cnt -eq 0 ]; then 
+    [ -f ~/nohup.out ] && rm -f ~/nohup.out
+    #nohup /tmp/tttt 2>/dev/null &
+    nohup /home/ubuntu/pcfconfig/pcfconfig $1 2>/dev/null &
+    echo $! > ~/pcfconfig.pid
+  fi
+else
+  [ -f ~/nohup.out ] && rm -f ~/nohup.out
+  #nohup /tmp/tttt 2>/dev/null &
+  nohup /home/ubuntu/pcfconfig/pcfconfig $1 2>/dev/null &
+  echo $! > ~/pcfconfig.pid
+fi
+
+sleep 2
+if [ -f ~/nohup.out ]; then
+  tail -100f ~/nohup.out
+fi
