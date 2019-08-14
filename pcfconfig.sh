0736327cd7bd6d1ebf265417c8e70c072354c780
Sacha Dubois <sadubois@pivotal.io>
2019-08-14 10:17:22 +0200

    z

--- pcfconfig.sh
+++ pcfconfig.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
+LOGFILE=/tmp/pcfconfig.log
+PIDFILE=/tmp/pcfconfig.pid
+COMMAND="~/pcfconfig/pcfconfig $*"
+COMMAND=/tmp/tttt 
+
 cd $HOME
-if [ -f ~/nohup.out ]; then
+if [ -f $LOGFILE ]; then
   cnt=$(egrep -c "################################ EOF ################################" ~/nohup.out)
   if [ $cnt -gt 0 ]; then 
-    cat ~/nohup.out
+    cat $LOGFILE
     exit 0
   fi
 fi
 
 if [ -f ~/pcfconfig.pid ]; then
-  read pid < ~/pcfconfig.pid
+  read pid < $PIDFILE
   cnt=$(ps -p $pid -o pid,comm | sed 1d | grep -c " ${pid} ") 
   if [ $cnt -eq 0 ]; then 
-    [ -f ~/nohup.out ] && rm -f ~/nohup.out
-    nohup /tmp/tttt 2>/dev/null &
-    #nohup ~/pcfconfig/pcfconfig $* 2>/dev/null &
-    echo $! > ~/pcfconfig.pid
+    nohup $COMMAND > $LOGFILE 2>&1 &
+    echo $! > $PIDFILE
   fi
 else
-  [ -f ~/nohup.out ] && rm -f ~/nohup.out
-  nohup /tmp/tttt 2>/dev/null &
-  #nohup ~/pcfconfig/pcfconfig $* 2>/dev/null &
-  echo $! > ~/pcfconfig.pid
+  nohup $COMMAND > $LOGFILE 2>&1 &
+  echo $! > $PIDFILE
 fi
 
 sleep 2
