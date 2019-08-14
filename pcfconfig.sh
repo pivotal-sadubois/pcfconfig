b5d9b283b5dd9578501b123926c4d073ab054734
Sacha Dubois <sadubois@pivotal.io>
2019-08-14 14:03:15 +0200

    fixes on AWS jump

--- pcfconfig.sh
+++ pcfconfig.sh
@@ -5,9 +5,11 @@ PIDFILE=/tmp/pcfconfig.pid
 COMMAND=/tmp/tttt 
 COMMAND="$HOME/pcfconfig/pcfconfig $*"
 
+eof=0
+
 if [ -f $LOGFILE ]; then
-  cnt=$(egrep -c "################################ EOF ################################" $LOGFILE)
-  if [ $cnt -gt 0 ]; then 
+  eof=$(egrep -c "################################ EOF ################################" $LOGFILE)
+  if [ $eof -gt 0 ]; then 
     cat $LOGFILE
   fi
   exit
@@ -17,12 +19,11 @@ if [ -f $PIDFILE ]; then
   stt=$(pgrep -F /tmp/pcfconfig.pid)
   if [ "$stt" == "" ]; then 
     echo "$0 NOT RUNNING, RESTARTING"
-    date +%s > $LOGFILE
     nohup $COMMAND >> $LOGFILE 2>&1 &
     echo $! > $PIDFILE
   fi
 else
-  echo "$0 1NOT RUNNING, RESTARTING"
+  echo "$0 WAS NEVER RUNNING, 1st START"
   date +%s > $LOGFILE
   nohup $COMMAND >> $LOGFILE 2>&1 &
   echo $! > $PIDFILE
