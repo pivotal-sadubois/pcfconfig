24b20b3ac20d6a14d337f5100f42b8518c4ccaea
Sacha Dubois <sadubois@pivotal.io>
2019-08-15 21:52:17 +0200

    debug cleanup

--- pcfconfig.sh
+++ pcfconfig.sh
@@ -3,7 +3,8 @@
 LOGFILE=/tmp/pcfconfig.log
 PIDFILE=/tmp/pcfconfig.pid
 COMMAND=/tmp/tttt 
-COMMAND="$HOME/pcfconfig/pcfconfig $*"
+COMMAND=$HOME/pcfconfig/pcfconfig
+ARGS="$*"
 echo "COMMAND:$COMMAND"
 
 eof=0
@@ -20,13 +21,13 @@ if [ -f $PIDFILE ]; then
   stt=$(pgrep -F /tmp/pcfconfig.pid)
   if [ "$stt" == "" ]; then 
     echo "$0 NOT RUNNING, RESTARTING"
-    nohup "$COMMAND" >> $LOGFILE 2>&1 &
+    nohup $COMMAND "$ARGS" >> $LOGFILE 2>&1 &
     echo $! > $PIDFILE
   fi
 else
   echo "$0 WAS NEVER RUNNING, 1st START"
   date +%s > $LOGFILE
-  nohup "$COMMAND" >> $LOGFILE 2>&1 &
+  nohup $COMMAND "$ARGS" >> $LOGFILE 2>&1 &
   echo $! > $PIDFILE
 fi
 
