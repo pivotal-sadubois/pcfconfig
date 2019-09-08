c19292f0c71c43ed1786150c7bc9f1fcf9086342
Sacha Dubois <sadubois@pivotal.io>
2019-09-08 14:47:11 +0200

    debug azure login

--- scripts/checkAZlogin.sh
+++ scripts/checkAZlogin.sh
@@ -3,9 +3,8 @@
 stt=0
 while [ $stt -ne 0 ]; do
   az group list > /dev/null 2>&1; stt=$?
-  if [ stt -ne 0 ]; then
-    az login
-    stt=$?
+  if [ $stt -ne 0 ]; then
+    az login > /dev/null 2>&1
   fi
 done
 
