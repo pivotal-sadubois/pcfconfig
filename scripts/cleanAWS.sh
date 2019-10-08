e9d11c68d39e6974ad20657c815a253d8dac29fa
Sacha Dubois <sadubois@pivotal.io>
2019-10-08 21:14:26 +0200

    added trusted_certificates config

--- scripts/cleanAWS.sh
+++ scripts/cleanAWS.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
+# ############################################################################################
+# File: ........: cleanAWS.sh
+# Language .....: bash
+# Author .......: Sacha Dubois, Pivotal
+# Cathegory ....: PCF Installation and Configuration Utility
+# Description ..: Clean AWS Environment 
+# ############################################################################################
+
+# --- CHECK ENVIRONMENT VARIABLES ---
+if [ -f ~/.pcfconfig ]; then
+  . ~/.pcfconfig
+fi
+
 if [ "$1" == "" ]; then
   echo "USAGE: $0 <env> <region>"
   echo "       $0 awspks eu-central-1"
+  exit 0
+fi
+
+if [ "$AWS_HOSTED_DNS_DOMAIN" == "" ]; then
+  echo "ERROR: DNS Domain not specified."
+  echo "       => export AWS_HOSTED_DNS_DOMAIN=<domain>"
   exit 0
 fi
 
