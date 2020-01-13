c14f91a47e8bdeedd7c8eeca17614bfdaf4a9f70
Sacha Dubois <sadubois@pivotal.io>
2020-01-14 00:34:33 +0100

    testing

--- demos/pbs-spring-petclinic/Build_PetClinic_PKS.sh
+++ demos/pbs-spring-petclinic/Build_PetClinic_PKS.sh
@@ -98,8 +98,6 @@ if [ "$PB" == "" ]; then
   exit
 fi
 
-HARBOR_REGISTRY="harbor.${PCF_DEPLOYMENT_ENV_NAME}.${AWS_HOSTED_DNS_DOMAIN}"
-HARBOR_PROJECT=library
 PCF_TILE_HARBOR_ADMIN_USER=admin
 
 missing_variables=0
@@ -132,45 +130,83 @@ else
 fi
 
 if [ $REGISTRY_GOHARBOR -eq 1 ]; then 
-  if [ "${PCF_PBS_GOHARBOR_USER}" == "" -o "${PCF_PBS_GOHARBOR_PASS}" == "" -a "${PCF_PBS_GOHARBOR_PROJ}" == "" ]; then
+  CONTAINER_REGISTRY=demo.goharbor.io
+  CONTAINER_PROJECT=$PCF_PBS_GOHARBOR_PROJ
+  PBS_PROJECT=pet-clinic-goharbor
+
+  if [ "${PCF_PBS_GOHARBOR_USER}" == "" -o "${PCF_PBS_GOHARBOR_PASS}" == "" -o "${PCF_PBS_GOHARBOR_PROJ}" == "" -o \
+       "${PCF_PBS_GOHARBOR_EMAIL}" == "" -o "${PCF_PBS_GOHARBOR_FIRST_NAME}" == "" -o "${PCF_PBS_GOHARBOR_LAST_NAME}" == "" ]; then
     missing_variables=1
-    echo ""
     echo "  MISSING ENVIRONMENT-VARIABES  DESCRIPTION        "
     echo "  --------------------------------------------------------------------------------------------------------------"
   
     if [ "${PCF_PBS_GOHARBOR_USER}" == "" ]; then
-      echo "  PCF_TILE_HARBOR_ADMIN_USER    (required) Harbor Administrator User"
+      echo "  PCF_PBS_GOHARBOR_USER         (required) goHarbor User Name"
     fi
   
     if [ "${PCF_PBS_GOHARBOR_PASS}" == "" ]; then
-      echo "  PCF_TILE_HARBOR_ADMIN_PASS    (required) Harbor Administrator Password"
+      echo "  PCF_PBS_GOHARBOR_PASS         (required) goHarbor User Password"
+    fi
+  
+    if [ "${PCF_PBS_GOHARBOR_EMAIL}" == "" ]; then
+      echo "  PCF_PBS_GOHARBOR_EMAIL        (required) goHarbor User Email"
+    fi
+  
+    if [ "${PCF_PBS_GOHARBOR_FIRST_NAME}" == "" ]; then
+      echo "  PCF_PBS_GOHARBOR_FIRST_NAME   (required) goHarbor User First Name"
+    fi
+  
+    if [ "${PCF_PBS_GOHARBOR_LAST_NAME}" == "" ]; then
+      echo "  PCF_PBS_GOHARBOR_LAST_NAME    (required) goHarbor User Last Name"
     fi
   
     if [ "${PCF_PBS_GOHARBOR_PROJ}" == "" ]; then
-      echo "  PCF_TILE_HARBOR_ADMIN_PROJ    (required) Harbor Project"
+      echo "  PCF_TILE_HARBOR_ADMIN_PROJ    (required) goHarbor Project"
     fi
   
     echo ""
-    echo "  A user account for the hosted harbor registry (https://$HARBOR_REGISTRY) can be optained under the"
+    echo "  A user account for the hosted harbor registry (https://$CONTAINER_REGISTRY) can be optained under the"
     echo "  following link: https://github.com/goharbor/harbor/blob/master/docs/demo_server.md"
-  else
-    messageTitle "Pivotal Container Platform (Harbor)"
-    messagePrint " - Harbor Registry:" "demo.goharbor.io"
-    messagePrint " - Harbor Administrator User"     "$PCF_PBS_GOHARBOR_USER"
-    messagePrint " - Harbor Administrator Password:" "##########"
     echo ""
+  else
+    if [ "${PCF_PBS_GOHARBOR_PROJ}" == "library" ]; then
+      echo "ERROR: The goHarbor Registry does not allow 'library' as project"
+      exit
+    fi
+  
+    messageTitle "  Harbor Container Registry"
+    messagePrint "   - Registry:"         "demo.goharbor.io"
+    messagePrint "   - Project:"          "$PCF_PBS_GOHARBOR_PROJ"
+    messagePrint "   - User Name:"        "$PCF_PBS_GOHARBOR_USER"
+    messagePrint "   - User Password:"    "##########"
+    messagePrint "   - User Email:"       "$PCF_PBS_GOHARBOR_EMAIL"
+    messagePrint "   - User First Name:"  "$PCF_PBS_GOHARBOR_FIRST_NAME"
+    messagePrint "   - User Last Name:"   "$PCF_PBS_GOHARBOR_LAST_NAME"
+    messagePrint "   - Project Name:"     "$PCF_PBS_GOHARBOR_PROJ"
+    echo ""
+
+    harborAPIuserAdd demo.goharbor.io $PCF_PBS_GOHARBOR_USER $PCF_PBS_GOHARBOR_PASS \
+                     $PCF_PBS_GOHARBOR_EMAIL $PCF_PBS_GOHARBOR_FIRST_NAME $PCF_PBS_GOHARBOR_LAST_NAME 
+
+    harborAPIprojectAdd demo.goharbor.io $PCF_PBS_GOHARBOR_PROJ $PCF_PBS_GOHARBOR_USER $PCF_PBS_GOHARBOR_PASS
+
+    #harborAPIuserUser demo.goharbor.io $PCF_PBS_GOHARBOR_USER $PCF_PBS_GOHARBOR_PASS \
+    #                  $PCF_PBS_GOHARBOR_EMAIL $PCF_PBS_GOHARBOR_FIRST_NAME $PCF_PBS_GOHARBOR_LAST_NAME 
   fi
 fi
 
 if [ $REGISTRY_DOCKER -eq 1 ]; then
+  PBS_PROJECT=pet-clinic-docker
+  CONTAINER_REGISTRY=$PCF_TILE_PBS_DOCKER_REPO
+  CONTAINER_PROJECT=$PCF_TILE_PBS_DOCKER_USER
+
   if [ "${PCF_TILE_PBS_DOCKER_REPO}" == "" -o "${PCF_TILE_PBS_DOCKER_USER}" == "" -o "${PCF_TILE_PBS_DOCKER_PASS}" == "" ]; then
-    echo ""
     echo "  MISSING ENVIRONMENT-VARIABES  DESCRIPTION        "
     echo "  --------------------------------------------------------------------------------------------------------------"
     echo ""
 
     if [ "${PCF_TILE_PBS_DOCKER_REPO}" == "" ]; then
-      echo "  PCF_TILE_PBS_DOCKER_REPO      (required) Docker Repository Name"
+      echo "  PCF_TILE_PBS_DOCKER_REPO      (required) Docker Registry"
     fi
 
     if [ "${PCF_TILE_PBS_DOCKER_USER}" == "" ]; then
@@ -185,17 +221,20 @@ if [ $REGISTRY_DOCKER -eq 1 ]; then
 
     variable_notice; exit 1
   else
-    messageTitle "  Docker Registry"
-    messagePrint "   - Harbor Registry:"              "$PCF_TILE_PBS_DOCKER_REPO
-    messagePrint "   - Harbor Administrator User"     "admin
-    messagePrint "   - Harbor Administrator Password" "##########"
+    messageTitle "  Docker Container Registry"
+    messagePrint "   - Registry URL:"          "$PCF_TILE_PBS_DOCKER_REPO"
+    messagePrint "   - User"                   "$PCF_TILE_PBS_DOCKER_USER"
+    messagePrint "   - Password"               "##########"
     echo ""
   fi
 fi
 
 if [ $REGISTRY_HARBOR -eq 1 ]; then 
-  if [ "${PCF_TILE_HARBOR_ADMIN_PASS}" == "" ]; then
-    echo ""
+  CONTAINER_REGISTRY="harbor.${PCF_DEPLOYMENT_ENV_NAME}.${AWS_HOSTED_DNS_DOMAIN}"
+  CONTAINER_PROJECT=$PCF_PBS_HARBOR_PROJ
+  PBS_PROJECT=pet-clinic-harbor
+
+  if [ "${PCF_TILE_HARBOR_ADMIN_PASS}" == "" -o "${PCF_PBS_HARBOR_PROJ}" == "" ]; then
     echo "  MISSING ENVIRONMENT-VARIABES  DESCRIPTION        "
     echo "  --------------------------------------------------------------------------------------------------------------"
     echo ""
@@ -205,20 +244,25 @@ if [ $REGISTRY_HARBOR -eq 1 ]; then
       echo ""
     fi
 
+    if [ "${PCF_PBS_HARBOR_PROJ}" == "" ]; then
+      echo "  PCF_PBS_HARBOR_PROJ           (required) Harbor Project"
+    fi
+  
     variable_notice; exit 1
   else
     messageTitle "  Pivotal Container Platform (Harbor)"
-    messagePrint "   - Harbor Registry:" "$HARBOR_REGISTRY"
+    messagePrint "   - Harbor Registry:" "$CONTAINER_REGISTRY"
     messagePrint "   - Harbor Administrator User" "admin"
     messagePrint "   - Harbor Administrator Password" "##########"
     echo ""
+
+    harborAPIprojectAdd $CONTAINER_REGISTRY $PCF_PBS_HARBOR_PROJ admin $PCF_TILE_HARBOR_ADMIN_PASS
   fi
 fi
 
 if [ "${PCF_TILE_PBS_ADMIN_USER}" == "" -o "${PCF_TILE_PBS_ADMIN_PASS}" == "" -o "${PCF_TILE_PBS_GITHUB_REPO}" == "" -o \
      "${PCF_TILE_PBS_GITHUB_USER}" == "" -o "${PCF_TILE_PBS_GITHUB_PASS}" == "" ]; then
 
-  echo ""
   echo "  MISSING ENVIRONMENT-VARIABES  DESCRIPTION        "
   echo "  --------------------------------------------------------------------------------------------------------------"
 
@@ -255,25 +299,6 @@ else
 fi
 
 
-if [ $REGISTRY_DOCKER -eq 1 ]; then 
-  echo "registry: $PCF_TILE_PBS_DOCKER_REPO"    >  /tmp/docker.yml
-  echo "username: $PCF_TILE_PBS_DOCKER_USER"    >> /tmp/docker.yml
-  echo "password: $PCF_TILE_PBS_DOCKER_PASS"    >> /tmp/docker.yml
-fi
-
-if [ $REGISTRY_GOHARBOR -eq 1 ]; then 
-  echo "registry: $HARBOR_REGISTRY"             >  /tmp/harbor.yml
-  echo "username: $PCF_PBS_GOHARBOR_USER"       >> /tmp/harbor.yml
-  echo "password: $PCF_PBS_GOHARBOR_PASS"       >> /tmp/harbor.yml
-fi
-
-if [ $REGISTRY_HARBOR -eq 1 ]; then 
-  echo "registry: ${HARBOR_REGISTRY}"           >  /tmp/harbor.yml
-  echo "username: $PCF_TILE_HARBOR_ADMIN_USER"  >> /tmp/harbor.yml
-  echo "password: $PCF_TILE_HARBOR_ADMIN_PASS"  >> /tmp/harbor.yml
-fi
-exit 
-
 echo "registry: $PCF_TILE_PBS_GITHUB_REPO"    >  /tmp/github.yml
 echo "username: $PCF_TILE_PBS_GITHUB_USER"    >> /tmp/github.yml
 echo "password: $PCF_TILE_PBS_GITHUB_PASS"    >> /tmp/github.yml
@@ -287,40 +312,80 @@ execCmd "pb api set ${PB_API_TARGET} --skip-ssl-validation"
 execCmd "pb login"
 
 prtHead "Create and select Project pet-clinic"
-execCmd "pb project create pet-clinic-harbor 2>/dev/null"
-execCmd "pb project target pet-clinic-harbor"
+execCmd "pb project create $PBS_PROJECT 2>/dev/null"
+execCmd "pb project target $PBS_PROJECT"
 
-prtHead "Add screts for Harbor Registry from (/tmp/harbor.yml)" 
-execCmd "cat /tmp/harbor.yml | sed '/^password: /s/.*/password: xxxxxxxx/g'"
-execCmd "pb secrets registry apply -f /tmp/harbor.yml"
+if [ $REGISTRY_DOCKER -eq 1 ]; then 
+  echo "registry: $PCF_TILE_PBS_DOCKER_REPO"    >  /tmp/docker.yml
+  echo "username: $PCF_TILE_PBS_DOCKER_USER"    >> /tmp/docker.yml
+  echo "password: $PCF_TILE_PBS_DOCKER_PASS"    >> /tmp/docker.yml
+
+  prtHead "Add screts for Docker Registry from (/tmp/docker.yml)"
+  execCmd "cat /tmp/docker.yml | sed '/^password: /s/.*/password: xxxxxxxx/g'"
+  execCmd "pb secrets registry apply -f /tmp/docker.yml"
+fi
+
+if [ $REGISTRY_GOHARBOR -eq 1 ]; then 
+  echo "registry: $CONTAINER_REGISTRY"             >  /tmp/harbor.yml
+  echo "username: $PCF_PBS_GOHARBOR_USER"       >> /tmp/harbor.yml
+  echo "password: $PCF_PBS_GOHARBOR_PASS"       >> /tmp/harbor.yml
+
+  prtHead "Add secrets for Harbor Registry from (/tmp/harbor.yml)"
+  execCmd "cat /tmp/harbor.yml | sed '/^password: /s/.*/password: xxxxxxxx/g'"
+  execCmd "pb secrets registry apply -f /tmp/harbor.yml"
+fi
+
+if [ $REGISTRY_HARBOR -eq 1 ]; then
+  echo "registry: ${CONTAINER_REGISTRY}"           >  /tmp/harbor.yml
+  echo "username: $PCF_TILE_HARBOR_ADMIN_USER"  >> /tmp/harbor.yml
+  echo "password: $PCF_TILE_HARBOR_ADMIN_PASS"  >> /tmp/harbor.yml
+
+  prtHead "Add secrets for Harbor Registry from (/tmp/harbor.yml)" 
+  execCmd "cat /tmp/harbor.yml | sed '/^password: /s/.*/password: xxxxxxxx/g'"
+  execCmd "pb secrets registry apply -f /tmp/harbor.yml"
+fi
 
 prtHead "Add screts for Docker Registry from (/tmp/github.yml)" 
 execCmd "cat /tmp/github.yml | sed '/^password: /s/.*/password: xxxxxxxx/g'"
 execCmd "pb secrets registry apply -f /tmp/github.yml"
 
-sed -e "s+XXXGITREPOSITORYXXX+${PCF_TILE_PBS_DEMO_PETCLINIC}+g" -e "s/XXXDOMAINXXX/${HARBOR_REGISTRY}/g" \
-    -e "s/YYYREPOYYY/${HARBOR_PROJECT}/g" \
-    files/spring-petclinic-harbor-template.yml > /tmp/spring-petclinic-harbor.yml
+sed -e "s+XXXGITREPOSITORYXXX+${PCF_TILE_PBS_DEMO_PETCLINIC}+g" -e "s/XXXDOMAINXXX/${CONTAINER_REGISTRY}/g" \
+    -e "s/YYYREPOYYY/${CONTAINER_PROJECT}/g" \
+    files/spring-petclinic-harbor-template.yml > /tmp/spring-petclinic.yml
 
-prtHead "Create Image (/tmp/spring-petclinic-harbor.yml)"
-execCmd "cat /tmp/spring-petclinic-harbor.yml"
-execCmd "pb image apply -f /tmp/spring-petclinic-harbor.yml"
+prtHead "Create Image (/tmp/spring-petclinic.yml)"
+execCmd "cat /tmp/spring-petclinic.yml"
+execCmd "pb image apply -f /tmp/spring-petclinic.yml"
 sleep 7
 execCmd "pb image list"
-execCmd "pb image builds ${HARBOR_REGISTRY}/${HARBOR_PROJECT}/spring-petclinic:latest"
+execCmd "pb image builds ${CONTAINER_REGISTRY}/${CONTAINER_PROJECT}/spring-petclinic:latest"
 
-bld=$(pb image builds ${HARBOR_REGISTRY}/${HARBOR_PROJECT}/spring-petclinic:latest | \
+bld=$(pb image builds ${CONTAINER_REGISTRY}/${CONTAINER_PROJECT}/spring-petclinic:latest | \
       sed '/^$/d' | tail -1 | awk '{ print $1 }') 
-execCmd "pb image logs ${HARBOR_REGISTRY}/${HARBOR_PROJECT}/spring-petclinic:latest -b $bld -f"
+execCmd "pb image logs ${CONTAINER_REGISTRY}/${CONTAINER_PROJECT}/spring-petclinic:latest -b $bld -f"
 
-prtText "Login in to Harbor Repository on ($HARBOR_REGISTRY)"
-prtText " => https://$HARBOR_REGISTRY"
-prtText "Deploy the container to PKS"
-prtText " => deploy-petclinic-harbor-k8s.sh"
+if [ $REGISTRY_GOHARBOR -eq 1 ]; then
+  prtText "Login in to Harbor Registry on ($CONTAINER_REGISTRY)"
+  prtText " => https://$CONTAINER_REGISTRY"
+fi
+
+if [ $REGISTRY_HARBOR -eq 1 ]; then
+  prtText "Login in to Harbor Registry on ($CONTAINER_REGISTRY)"
+  prtText " => https://$CONTAINER_REGISTRY"
+fi
+
+if [ $REGISTRY_DOCKER -eq 1 ]; then
+  prtText "Login in to Docker Registry on ($CONTAINER_REGISTRY)"
+  prtText " => https://$PCF_TILE_PBS_DOCKER_REPO"
+fi
+
+prtText "Deploy the container to Docker/Kubernets"
+prtText " => Deploy_PetClinic_K8s.sh"
+prtText " => Deploy_PetClinic_Docker.sh"
 
 
 #prtText "Login in to Docker Repository on your local workstartion and run petclinic"
-#prtText " => sudo docker login ${HARBOR_REGISTRY} -u $PCF_TILE_HARBOR_ADMIN_USER -p $PCF_TILE_HARBOR_ADMIN_PASS"
+#prtText " => sudo docker login ${CONTAINER_REGISTRY} -u $PCF_TILE_HARBOR_ADMIN_USER -p $PCF_TILE_HARBOR_ADMIN_PASS"
 #prtText " => sudo docker run -e \"SPRING_PROFILES_ACTIVE=prod\" -p 8080:8080 -t --name springboot-petclinic ${PCF_TILE_PBS_DOCKER_REPO}/${PCF_TILE_PBS_DOCKER_USER}/spring-petclinic:latest"
 
 
